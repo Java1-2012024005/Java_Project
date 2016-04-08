cf33cdb56f58ed3343365c480485ccefc7e2a0d7
김병무 <@155.230.64.183>
2016-04-08 10:20:32 +0900

    숙제

---
+++ Java_Project/src/Homework_160411/homework.java
@@ -0,0 +1,19 @@
+package Homework_160411;
+
+public class homework {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		class Television {
+			int channel;
+			int volume;
+			boolean onOff;
+		}
+		Television tv = new Television();
+		tv.channel = 7;
+		tv.volume = 9;
+		tv.onOff = true;
+		System.out.println("텔레비전의 채널은" + tv.channel + "이고 볼륨은" + tv.volume + "입니다.");
+	}
+
+}
